53b7e85996d1f135527df7a756e2f3c6132e3687
bernadette <aguirre.bernadette@outlook.com>
2022-06-04 21:45:09 -0600

    Completed Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,17 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+RMFunctions.wildlife();
 
-  return <h1>Colorado State Parks!</h1>;
+RMFunctions.elevation();
+
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  ); 
 }
+
